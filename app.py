e2958dea1e4625b4bfbaebcbc732ca7c1f9983bb
Ema Ikeda <ema.ikeda@bellevuecollege.edu>
2025-11-13 17:20:14 -0800

    Filter recipes based on excluded ingredients

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 from utils.data import load_and_preprocess_data
-# from utils.recommender import filter_recipes, get_excluded_ingredients
+from utils.recommender import filter_recipes, get_excluded_ingredients
 
 st.title("Recipe Recommender")
 
@@ -8,23 +8,24 @@ st.title("Recipe Recommender")
 df = load_and_preprocess_data()
 st.write(df.head(5))
 
-# # Prompt the user to enter several ingredients and dietary restrictions. Conver this to individual ingredients to exclude in the recipe.
-# ingredients_input = input("Enter the ingredients (comma separated): ")
-# included_ingredients = [token.strip().lower() for token in ingredients_input.split(',') if token.strip()]
+# Prompt the user to enter several ingredients and dietary restrictions. Conver this to individual ingredients to exclude in the recipe.
+ingredients_input = st.text_input("Enter the ingredients (comma separated): ")
+included_ingredients = [token.strip().lower() for token in ingredients_input.split(',') if token.strip()]
 
-# excluded_input = input("Enter ingredients to exclude. You can also enter dietary restrictions (comma separated): ")
-# excluded_tokens = [token.strip().lower() for token in excluded_input.split(',') if token.strip()]
+excluded_input = st.text_input("Enter ingredients to exclude. You can also enter dietary restrictions (comma separated): ")
+excluded_tokens = [token.strip().lower() for token in excluded_input.split(',') if token.strip()]
 
-# selected_diets = [] # ingredients excluded under known dietary restrictions
+selected_diets = [] # ingredients excluded under known dietary restrictions
 
-# for token in excluded_tokens:
-#   selected_diets.append(token)
+for token in excluded_tokens:
+  selected_diets.append(token)
 
-# excluded_ingredients = get_excluded_ingredients(selected_diets)
+excluded_ingredients = get_excluded_ingredients(selected_diets)
 
-# print(f"\nIngredients to include: {included_ingredients}")
-# print(f"Ingredients to exclude (including dietary restrictions): {sorted(excluded_ingredients)}")
+st.write(f"\nIngredients to include: {included_ingredients}")
+st.write(f"Ingredients to exclude (including dietary restrictions): {sorted(excluded_ingredients)}")
 
-# filter_recipes(df['ingredients_clean'], excluded_ingredients)
+filtered_recipes = filter_recipes(df, excluded_ingredients)
+st.write(filtered_recipes.head())
 # # top_5_recipes = recommend_recipe(filter_recipes(df['ingredients_clean'], excluded_ingredients), included_ingredients)
 # # print(top_5_recipes)
---
+++ utils/recommender.py
@@ -0,0 +1,69 @@
+# Core logic of the app. Covers filtering, recommending, and similarity scoring of the recipes
+
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+from utils.constants import DIETARY_RESTRICTIONS, INGREDIENT_TRAITS
+
+"""
+Accepts a list of user-provided restrictions (e.g. 'vegan', 'dairy', 'soy')
+and returns a full list of excluded ingredients.
+"""
+def get_excluded_ingredients(selected_diets):
+  excluded = set()
+  for item in selected_diets:
+    item = item.lower().strip()
+
+    if item in DIETARY_RESTRICTIONS:
+      traits = DIETARY_RESTRICTIONS.get(item, [])
+      for trait in traits:
+          excluded.update(INGREDIENT_TRAITS.get(trait, []))
+
+    elif item in INGREDIENT_TRAITS:
+      excluded.update(INGREDIENT_TRAITS[item])
+
+    else:
+      excluded.add(item)
+  return excluded
+
+"""
+Return a DataFrame with recipes that do NOT contain any excluded ingredients.
+
+Args:
+    df (DataFrame): The full recipes dataset with 'ingredients_clean' column.
+    excluded_ingredients (list or set): Ingredients to avoid.
+
+Returns:
+    DataFrame: Filtered recipes.
+"""
+def filter_recipes(df, excluded_ingredients):
+    excluded_lower = [ex.lower() for ex in excluded_ingredients]
+    # Boolean mask: True for rows we want to keep
+    mask = df['ingredients_clean'].apply(
+        lambda ingr_list: not any(ex in ing.lower() for ing in ingr_list for ex in excluded_lower)
+    )
+    # Return copy of masked dataframe
+    return df[mask].copy()
+
+"""
+    Recommend top 5 recipes based on user-provided ingredients and dietary restrictions.
+    This function assumes that recipes are filtered to remove recipes that contain excluded ingredients.
+    Uses bag-of-words model with cosine similarity to match the user's desired ingredients against the ingredient list of recipes.
+
+    Args:
+       1. recipes (list of dict): Recipe dataset filtered by excluded ingredients
+       2. included_ingredients (list of str): Ingredients the user wants to include
+    Returns:
+       List of dict: Top 5 recipe dictionaries ranked by similarity to the included ingredients.
+"""
+def recommend_recipe(recipes, included_ingredients):
+  texts = [' '.join(r['ingredients']) for r in recipes]
+
+  vectorizer = CountVectorizer()
+  recipe_vectors = vectorizer.fit_transform(texts)
+
+  user_query = ' '.join(included_ingredients)
+  user_vector = vectorizer.transform([user_query])
+
+  similarities = cosine_similarity(user_vector, recipe_vectors).flatten()
+  ranked = sorted(zip(recipes, similarities), key=lambda x:x[1], reverse=True)
+  return ranked[:5]
