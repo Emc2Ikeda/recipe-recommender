696bb22f66cf1fb3e82474e24b3e16ee0b37fe14
Ema Ikeda <ema.ikeda@bellevuecollege.edu>
2025-11-13 16:21:51 -0800

    Make the app load preprocessed DataFrame

--- app.py
+++ app.py
@@ -1,3 +1,30 @@
 import streamlit as st
-from utils import recipe_recommender
+from utils.data import load_and_preprocess_data
+# from utils.recommender import filter_recipes, get_excluded_ingredients
+
 st.title("Recipe Recommender")
+
+# load preprocessed data
+df = load_and_preprocess_data()
+st.write(df.head(5))
+
+# # Prompt the user to enter several ingredients and dietary restrictions. Conver this to individual ingredients to exclude in the recipe.
+# ingredients_input = input("Enter the ingredients (comma separated): ")
+# included_ingredients = [token.strip().lower() for token in ingredients_input.split(',') if token.strip()]
+
+# excluded_input = input("Enter ingredients to exclude. You can also enter dietary restrictions (comma separated): ")
+# excluded_tokens = [token.strip().lower() for token in excluded_input.split(',') if token.strip()]
+
+# selected_diets = [] # ingredients excluded under known dietary restrictions
+
+# for token in excluded_tokens:
+#   selected_diets.append(token)
+
+# excluded_ingredients = get_excluded_ingredients(selected_diets)
+
+# print(f"\nIngredients to include: {included_ingredients}")
+# print(f"Ingredients to exclude (including dietary restrictions): {sorted(excluded_ingredients)}")
+
+# filter_recipes(df['ingredients_clean'], excluded_ingredients)
+# # top_5_recipes = recommend_recipe(filter_recipes(df['ingredients_clean'], excluded_ingredients), included_ingredients)
+# # print(top_5_recipes)
--- utils/data.py
+++ utils/data.py
@@ -21,32 +21,16 @@ def delete_duplicate(df):
     name_counts = df['recipe_name'].value_counts()
     duplicates = name_counts[name_counts > 1]
 
-    # # Filter rows with duplicate recipe names
+    # Filter rows with duplicate recipe names
     df_duplicates = df[df['recipe_name'].isin(duplicates.index)]
     df_duplicates = df_duplicates.sort_values('recipe_name')
-    df_duplicates[['recipe_name', 'ingredients']].tail(10)
-
-    # check if duplicate recipes also have the same ingredient list (true duplicates)
-
-    # Group by recipe name and count unique ingredients per group
-    duplicates_with_diff = df.groupby('recipe_name')['ingredients'].nunique()
-
-    # Filter to show recipe names that have more than 1 unique ingredients entry
-    conflicting_duplicates = duplicates_with_diff[duplicates_with_diff > 1]
 
     # Since all the recipes with duplicate names also have duplicate ingredient list, drop recipes with duplicate names.
     df = df.drop_duplicates(subset='recipe_name').reset_index(drop=True)
-    print(f"Remaining duplicate names: {df['recipe_name'].duplicated().sum()}")
     return df
 
-df = load_data()
-print(len(df))
-
-deleted_duplicate_df = delete_duplicate(df)
-print(len(deleted_duplicate_df))
-
-# Format ingredients column to a list
-def get_ingredients_list(df):
+# Format ingredients column to a list and add to the DataFrame
+def add_ingredients_clean(df):
     cleaned_ingredients = []
 
     for row in df['ingredients']:
@@ -56,7 +40,10 @@ def get_ingredients_list(df):
         cleaned_ingredients.append(ingredients)
 
     df['ingredients_clean'] = cleaned_ingredients
-    return cleaned_ingredients
 
-cleaned_ingredients = get_ingredients_list(df)
-print(cleaned_ingredients)
+# Return preprocessed data to Streamlit app
+def load_and_preprocess_data():
+    df = load_data()
+    df = delete_duplicate(df)
+    add_ingredients_clean(df)
+    return df
